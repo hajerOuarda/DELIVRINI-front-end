ccc32aebf98dd438b7fa3dce3d2ecd93b215129c
hajer.ouarda <hajer.ouarda@gmail.com>
2022-04-27 13:05:04 +0100

    resolved protectedRoute problem

--- delivrini-front/src/routes/protectedRoutes.tsx
+++ delivrini-front/src/routes/protectedRoutes.tsx
@@ -1,11 +1,12 @@
-import { Route } from "react-router-dom";
+import { Children } from "react";
+import { Navigate, Outlet, Route } from "react-router-dom";
 import { NotAuthorizedPage } from "../pages/NotAuthorizedPage";
 import { paths } from "../utils/enums/routes";
 
-export const AuthenticatedRoute = (...rest: any) => {
+export const AuthenticatedRoute = ({ children }: any) => {
   const isAuthenticated = localStorage.getItem("token");
   console.log("token :", isAuthenticated);
 
-  if (isAuthenticated) return <Route {...rest} />;
-  else return <Route path={paths.not_authorized} element={<NotAuthorizedPage/>} />;
+  if (isAuthenticated) return children;
+  else return <Navigate to={paths.not_authorized} />;
 };
