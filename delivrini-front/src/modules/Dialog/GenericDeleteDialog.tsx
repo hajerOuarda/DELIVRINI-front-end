32c8a433dc1803609d4f573f996c798bde9a9c56
hajer.ouarda <hajer.ouarda@gmail.com>
2022-06-15 14:55:07 +0100

    created generic Delete Dialog

---
+++ delivrini-front/src/modules/Dialog/GenericDeleteDialog.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+
+export function GenericDeleteDialog() {
+
+    return (<Typography color="textSecondary"> Are you sure you want to delete this item ?</Typography>)
+}
